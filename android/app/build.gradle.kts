ade589721efb35af3e06351693381b3a2739b59f
Mustafa Dagdeviren <usta360@mustafadagdeviren.com>
2026-03-06 23:19:46 +0300

    Usta360 Rebranding: Updated Android application ID and namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.portfoy360.portfoy360"
+    namespace = "com.usta360pro.com"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.portfoy360.portfoy360"
+        applicationId = "com.usta360pro.com"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/portfoy360/portfoy360/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.portfoy360.portfoy360
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
